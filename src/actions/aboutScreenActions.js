5af32361f90205ba385126d1dbb12506ebde7673
Ararat7 <ararat.cpp@gmail.com>
2018-02-23 18:41:46 +0400

    Cleanup.

--- src/actions/aboutScreenActions.js
+++
@@ -1,8 +0,0 @@
-const SET_POSITION = 'SET_POSITION';
-
-export const setPosition = (position) => {
-    return {
-        type: SET_POSITION,
-        position,
-    };
-};
--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -3,13 +3,11 @@ import {combineReducers} from 'redux';
 import NavReducer from './navReducer';
 import MainScreenReducer from './mainScreenReducer';
 import LoginScreenReducer from './loginScreenReducer';
-import AboutScreenReducer from './aboutScreenReducer';
 import CommonReducer from './commonReducer';
 
 export default AppReducer = combineReducers({
     nav: NavReducer,
     loginScreen: LoginScreenReducer,
     mainScreen: MainScreenReducer,
-    aboutScreen: AboutScreenReducer,
     common: CommonReducer,
 });
